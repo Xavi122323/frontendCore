b994c4e809591cd47aee16a85ba5c6caf6eacdaa
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-11-13 23:29:36 -0500

    sign in component

--- src/app/services/authenticator.service.ts
+++ src/app/services/authenticator.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticatorService {
 
-  constructor() { }
+  url:string = "http://127.0.0.1:3000";
+  constructor(private http: HttpClient) { }
+
+  proceedLogin(email: any, password: any){
+    const body = {
+      user: {
+        email: email,
+        password: password,
+      },
+    };
+    return this.http.post(this.url+'/api/v1/sign_in', body)
+  }
+
+  registerLogin(email: any, password: any){
+    const body = {
+      user: {
+        email: email,
+        password: password,
+      },
+    };
+    return this.http.post(this.url+'/api/v1/sign_up', body)
+  }
+
+  isLoggedIn(){
+    return localStorage.getItem('token')!=null;
+  }
 }
+
