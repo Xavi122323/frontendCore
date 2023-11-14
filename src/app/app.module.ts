2eaeb01192c978673bb364ad126edbe8cd4592bc
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-11-13 20:02:47 -0500

    sign in, register, auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { ListServidoresComponent } from './servidor/list-servidores/list-servidores.component';
 import { CreateServidorComponent } from './servidor/create-servidor/create-servidor.component';
 import { EditServidorComponent } from './servidor/edit-servidor/edit-servidor.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListServidoresComponent,
     CreateServidorComponent,
-    EditServidorComponent
+    EditServidorComponent,
+    SignInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
