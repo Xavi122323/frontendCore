0d9c9601a33d288e6de63ac22dd88e56b5810b6e
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-12-29 10:29:42 -0500

    uso cpu graph

--- src/app/consultas/uso-cpu/uso-cpu.component.ts
+++ src/app/consultas/uso-cpu/uso-cpu.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { ConsultasService } from 'src/app/services/consultas.service';
 import { ServidorService } from 'src/app/services/servidor.service';
-import { Chart, registerables } from 'chart.js';
+//import { Chart } from 'chart.js';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-uso-cpu',
@@ -15,14 +16,39 @@ export class UsoCPUComponent {
   endDate: string = '';
   chartData: any;
   servidor: any;
+  metricas: any = [];
+  chart: Chart | null = null;
+  errorMessage: string = '';
+  averageCpuUsage: number | null = null;
 
   constructor(private consultasService: ConsultasService, private servidorService: ServidorService) {}
 
   fetchData() {
     this.consultasService.getAverageCpuUsage(this.serverId, this.startDate, this.endDate)
-      .subscribe(data => {
-        console.log(data);
-        // Here you would process the data and update the chart
+      .subscribe({
+        next: (avgData) => {
+          this.averageCpuUsage = avgData.average_cpu;
+          this.errorMessage = '';
+        },
+        error: (err) => {
+          this.errorMessage = err.message;
+        }
+      });
+
+    this.consultasService.getCpuUsage(this.serverId, this.startDate, this.endDate)
+      .subscribe({
+        next: (data) => {
+          this.metricas = data;
+          this.createChart(this.metricas);
+          this.errorMessage = '';
+        },
+        error: (err) => {
+          this.errorMessage = err.message;
+          if (this.chart) {
+            this.chart.destroy();
+            this.chart = null;
+          }
+        }
       });
   }
 
@@ -32,6 +58,49 @@ export class UsoCPUComponent {
         this.servidor = servidor;
       }
     )
+  }
+
+  createChart(metricaData: any[]) {
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+      console.error('Rendering context not found');
+      return;
+    }
+  
+    if (!canvas) {
+      console.error('Canvas element not found');
+      return;
+    }
+
+    if (!Array.isArray(metricaData)) {
+      console.error('metricaData is not an array');
+      return;
+    }
+
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  
+    const labels = metricaData.map(row => row.fechaRecoleccion);
+    const data = metricaData.map(row => row.usoCPU);
+  
+    const chartData = {
+      labels: labels,
+      datasets: [{
+        label: 'CPU Usage',
+        data: data,
+      }]
+    };
+  
+    const config = {
+      type: 'line' as const,
+      data: chartData,
+      options: {}
+    };
+  
+    this.chart = new Chart(ctx, config);
   }
 
 }
--- src/app/services/consultas.service.ts
+++ src/app/services/consultas.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +17,38 @@ export class ConsultasService {
 
   getAverageCpuUsage(serverId: string, startDate: string, endDate: string): Observable<any> {
     const params = { server_id: serverId, start_date: startDate, end_date: endDate };
-    return this.http.get(this.url+'/api/v1/uso_cpu_promedio', { params });
+    return this.http.get(this.url+'/api/v1/uso_cpu_promedio', { params })
+    .pipe(
+      catchError((error) => {
+        if (error.status === 404) {
+          return throwError(() => new Error("No hay datos para las fechas ingresadas"));
+        }
+        else if(error.status === 400) {
+          return throwError(() => new Error("Error parametros faltantes por ingresar"));
+        } 
+        else {
+          return throwError(() => new Error("Error inesperado"));
+        }
+      })
+    );
+  }
+
+  getCpuUsage(serverId: string, startDate: string, endDate: string): Observable<any> {
+    const params = { server_id: serverId, start_date: startDate, end_date: endDate };
+    return this.http.get(this.url+'/api/v1/cpu_fechas', { params })
+    .pipe(
+      catchError((error) => {
+        if (error.status === 404) {
+          return throwError(() => new Error("No hay datos para las fechas ingresadas"));
+        }
+        else if(error.status === 400) {
+          return throwError(() => new Error("Error parametros faltantes por ingresar"));
+        } 
+        else {
+          return throwError(() => new Error("Error inesperado"));
+        }
+      })
+    );
   }
 
 }
