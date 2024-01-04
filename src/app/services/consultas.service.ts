1c3d83524971c365e7f932720ecfa8664221e1ba
Xavi122323 <xavier.ramos@udla.edu.ec>
2024-01-03 20:48:21 -0500

    render deploy

--- src/app/services/consultas.service.ts
+++ src/app/services/consultas.service.ts
@@ -11,8 +11,8 @@ export class ConsultasService {
 
   constructor(private http: HttpClient) { }
 
-  url:string = "http://127.0.0.1:3000";
-  //url:string = "https://corebackend.onrender.com";
+  //url:string = "http://127.0.0.1:3000";
+  url:string = "https://corebackend.onrender.com";
 
 
   getAverageCpuUsage(serverId: string, startDate: string, endDate: string): Observable<any> {
